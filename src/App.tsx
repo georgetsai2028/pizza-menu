85fb197e4e2a698d1a76889276965795b0c21bb8
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-03-21 18:08:18 -0700

    created header

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,24 @@ import spinachi from "./assets/pizzas/spinaci.jpg";
 export default function App() {
   return (
     <div>
-      Hello <Pizza />{" "}
+      <Header />
+      <Pizza />
     </div>
   );
 }
 
+const Header = () => {
+  return <h1> Puka's Pizzas </h1>;
+};
+
+const Menu = () => {};
+
+const Footer = () => {};
+
 function Pizza() {
   return (
     <div>
-      <img src={spinachi} />
+      <img src={spinachi} alt="spinachi pizza" />
       <h1> Pizza Spinachi</h1>
       <p> Tomato, Mozzarella, Spinach, and Ricotta Cheese </p>
     </div>
